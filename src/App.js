284466aa76e387a677534f8b0fb76a60b941d760
Stanislav Okunev <stanok3694@yandex.ru>
2018-10-29 22:59:26 +0300

    common: simplest error handling

--- src/App.js
+++ src/App.js
@@ -15,17 +15,25 @@ class App extends Component {
   componentDidMount() {
     fetch('https://api.treez.io/v1.0/dispensary/goe/menu/product_list?type=all&offset=0&limit=600"')
       .then(response => response.json())
-      .then(data => this.setState({ data, loading: false }))
+      .then(data => {
+        // just for fun -> i will handle this error from your super api ;)
+        if (data && data.error) {
+          this.setState({ errData: data.error, error: true})
+        } else {
+          this.setState({ data, loading: false })
+        }
+      })
       .catch(e => this.setState({ errData: e, error: true }));
   }
 
   render() {
-    const { error, loading, data } = this.state;
+    const { error, errData, loading, data } = this.state;
 
-    if (error) {
+    if (error || (data && data.error)) {
       return (
         <div>
           <h1>SOMETHING WENT WRONG!</h1>
+          <p>{errData}</p>
         </div>
       )
     }
@@ -41,7 +49,8 @@ class App extends Component {
     if (!loading && !error && data.length !== 0) {
       return (
         <div className="App">
-          {`here is data - ${data}`}
+          {/* here will be super styled list for you <3 */}
+          {`here is data - ${data}`} 
         </div>
       );
     }
