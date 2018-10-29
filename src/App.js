3a71822f38cec9d6f7395531c28770d26f4b6064
Stanislav Okunev <stanok3694@yandex.ru>
2018-10-29 22:47:51 +0300

    TASK: implemented data fetching (simplest)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      data: [],
+      error: false,
+      errData: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.treez.io/v1.0/dispensary/goe/menu/product_list?type=all&offset=0&limit=600"')
+      .then(response => response.json())
+      .then(data => this.setState({ data, loading: false }))
+      .catch(e => this.setState({ errData: e, error: true }));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { error, loading, data } = this.state;
+
+    if (error) {
+      return (
+        <div>
+          <h1>SOMETHING WENT WRONG!</h1>
+        </div>
+      )
+    }
+
+    if (loading) {
+      return (
+        <div>
+          <h1>LOADING, PLEASE WAIT...</h1>
+        </div>
+      )
+    }
+    
+    if (!loading && !error && data.length !== 0) {
+      return (
+        <div className="App">
+          {`here is data - ${data}`}
+        </div>
+      );
+    }
   }
 }
 
