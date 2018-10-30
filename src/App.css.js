d8a57fa23425eacc039a2676a0d96d912795ea64
Stanislav Okunev <stanok3694@yandex.ru>
2018-10-31 00:40:41 +0300

    TASK: styles refactored

---
+++ src/App.css.js
@@ -0,0 +1,61 @@
+export default {
+    flex_container: {
+        listStyle: 'none',
+        padding: 0,
+        margin: 0,
+        display: 'flex',
+        flexFlow: 'row wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      },
+      another_flex_container: {
+        display: 'flex',
+        flexFlow: 'column',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+      },
+      flex_item: {
+        backgroundColor: 'white',
+        padding: '5px',
+        width: '350px',
+        maxWidth: '75%',
+        maxHeight: '600px',
+        marginTop: '10px',
+        color: 'black',
+        // fontSize: '12pt',
+      },
+      image: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '60%',
+        height: '60%',
+      },
+      label: {
+        textTransform: 'uppercase',
+        height: '20px',
+        fontSize: '8pt',
+        border: 0,
+        borderRadius: '3pt',
+      },
+      type: {
+        background: 'lightgreen',
+        color: 'gray',
+        padding: '2px',
+      },
+      thc_percentage: {
+        color: 'grey',
+        backgroundColor: 'lightgray',
+      },
+      product_name: {
+        textTransform: 'uppercase',
+        fontSize: '18pt',
+        color: 'red',
+        fontWeight: 'bold',
+      },
+      brand_name: {
+        textTransform: 'uppercase',
+        fontSize: '8pt',
+        color: 'grey',
+      }      
+}
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,87 +3,84 @@ import PropTypes from "prop-types";
 import "../App.css";
 
 class ProductItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // should be stateless or not?
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      // should be stateless or not?
+    };
+  }
 
-    prepareSizePriceList(sizeList) {
-        return sizeList.map(elem => {
-            return {
-                size: elem.size,
-                price: elem.price_sell.toFixed(2)
-            }
-        });
-    }
+  prepareSizePriceList(sizeList) {
+    return sizeList.map(elem => {
+      return {
+        size: elem.size,
+        price: elem.price_sell.toFixed(2)
+      };
+    });
+  }
 
-    renderPrices(priceSizeList) {
-        return (
-            <div>
-                {
-                    priceSizeList.map((e, i) => {
-                        return (
-                            <div key={i}>
-                                <p>${e.price}</p>
-                                <p>/{e.size}</p>
-                            </div>
-                        )
-                    })
-                }
+  renderPrices(priceSizeList) {
+    return (
+      <div>
+        {priceSizeList.map((e, i) => {
+          return (
+            <div key={i}>
+              <p>${e.price}</p>
+              <p>/{e.size}</p>
             </div>
-        )
+          );
+        })}
+      </div>
+    );
+  }
+
+  getThcPercentage(thc_percentage) {
+    if (thc_percentage === "-.-") {
+      return null;
     }
 
-    getThcPercentage(thc_percentage) {
-        if (thc_percentage === '-.-') {
-            return null;
-        }
+    return <p className={"thc-percentage"}>THC {thc_percentage}%</p>;
+  }
 
-        return (
-            <p>{thc_percentage}</p>
-        )
-    }
+  getClassification(classifications) {
+    return (
+      <div>
+        {classifications.map((c, i) => {
+          return (
+            <p className="type" key={i}>
+              {c}
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
 
-    getClassification(classifications) {
-        return (
-            <div>
-                {
-                    classifications.map((c, i) => {
-                        return <p key={i}>{c}</p>
-                    })
-                }
-            </div>
-        );
-    }
+  render() {
+    const { data } = this.props;
 
-    render() {
-        const { data } = this.props;
-
-        return (
-            <li className="flex-item">
-                <img
-                    src={data.images.cropped_image}
-                    alt="product (cropped)"
-                    className="image"
-                />
-                <p>{data.product_name}</p>
-                <p>{data.brand}</p>
-                <p>{data.type}</p>
-                {this.getThcPercentage(data.attributes.thc_percentage)}
-                {this.getClassification(data.classifications)}
-                {
-                    this.renderPrices(this.prepareSizePriceList(data.size_list))
-                }
-
-            </li>
-        );
-    }
+    return (
+      <li className="flex-item">
+        <img
+          src={data.images.cropped_image}
+          alt="product (cropped)"
+          className="image"
+        />
+        <p className="brand-name">{data.brand}</p>
+        <p className="product-name">{data.product_name}</p>
+        <div className="flex-container">
+          <p className="type">{data.type}</p>
+          {this.getThcPercentage(data.attributes.thc_percentage)}
+          {this.getClassification(data.classifications)}
+          {this.renderPrices(this.prepareSizePriceList(data.size_list))}
+        </div>
+      </li>
+    );
+  }
 }
 
 ProductItem.propTypes = {
-    data: PropTypes.object,
-}
+  data: PropTypes.object
+};
 
 export default ProductItem;
